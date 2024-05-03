c434dbc63795384645c37ca0485166cad52e3bbe
tycannon1 <cannont41@gmail.com>
2024-05-02 18:12:21 -0600

    hamburger menu and dark mode

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -9,3 +9,23 @@ const lastModifiedDate = document.lastModified;
   
 // Populate the second paragraph with the last modified date
 document.getElementById("lastModified").textContent = `Last modified: ${lastModifiedDate}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        darkModeToggle.textContent = 'Light Mode';
+    } else {
+        darkModeToggle.textContent = 'Dark Mode';
+    }
+});
