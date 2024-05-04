b5ff57ec8e949b778052ac68a75f63d87638ce8c
tycannon1 <cannont41@gmail.com>
2024-05-04 17:48:01 -0600

    chamber home page

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,10 @@
+const currentYear = new Date().getFullYear();
+  
+// Populate the first paragraph with copyright info
+document.getElementById("copyright").innerHTML = `&copy; ${currentYear} Ty Cannon, USA`;
+  
+// Get the last modified date of the document
+const lastModifiedDate = document.lastModified;
+  
+// Populate the second paragraph with the last modified date
+document.getElementById("lastModified").textContent = `Last modified: ${lastModifiedDate}`;
