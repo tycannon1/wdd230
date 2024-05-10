a7c0e1292b1bc37484b4858681ed2c0cec852692
tycannon1 <cannont41@gmail.com>
2024-05-10 16:59:28 -0600

    fixed nav for home screen and homepage chamber

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,10 +1,30 @@
-const currentYear = new Date().getFullYear();
+// Wait for the DOM content to be fully loaded before executing JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
   
-// Populate the first paragraph with copyright info
-document.getElementById("copyright").innerHTML = `&copy; ${currentYear} Ty Cannon, USA`;
-  
-// Get the last modified date of the document
-const lastModifiedDate = document.lastModified;
-  
-// Populate the second paragraph with the last modified date
-document.getElementById("lastModified").textContent = `Last modified: ${lastModifiedDate}`;
+    const currentYear = new Date().getFullYear();
+    
+   
+    const copyrightElement = document.getElementById("copyright");
+    if (copyrightElement) {
+        copyrightElement.innerHTML = `&copy; ${currentYear} Ty Cannon, USA`;
+    }
+
+    const lastModifiedDate = document.lastModified;
+    
+   
+    const lastModifiedElement = document.getElementById("lastModified");
+    if (lastModifiedElement) {
+        lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
+    }
+
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
+
+    if (hamButton && navigation) {
+        hamButton.addEventListener('click', () => {
+            navigation.classList.toggle('open');
+            hamButton.classList.toggle('open');
+            document.querySelector('.close-icon').classList.toggle('show');
+        });
+    }
+});
